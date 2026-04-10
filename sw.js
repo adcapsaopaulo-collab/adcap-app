674344ee53c2921e94038584b8eb800cab4d775d
ADCAP_SPM <adcapsaopaulo@gmail.com>
2026-04-10 12:32:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,25 @@
 
-const CACHE_NAME = "adcap-pwa-v3";
-const urlsToCache = [
-  "/adcap-app/",
-  "/adcap-app/index.html"
-];
+const CACHE_NAME = "adcap-v2"; // MUDE SEMPRE QUE ATUALIZAR
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request));
 });
